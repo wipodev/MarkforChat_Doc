c48ea3d796d4c571e44cd50636fa4c0735933341
AJWipo <ajwipo@gmail.com>
2023-01-30 17:43:58 -0500

    agregado publicidad de ejemplo

---
+++ src/components/Advertising.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { mobile } from "../store/Utils";
+</script>
+
+<section class:active={$mobile}>
+  <img src="./publicidad.jpeg" alt="publicidad" />
+  <img src="./publicidad.jpeg" alt="publicidad" />
+</section>
+
+<style>
+  section {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    font-size: 2rem;
+    display: none;
+  }
+
+  .active {
+    display: block;
+  }
+
+  img {
+    width: auto;
+    height: 100%;
+  }
+</style>
--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -6,6 +6,7 @@
   import BtnNumber from "./BtnNumber.svelte";
   import BtnOpenChat from "./BtnOpenChat.svelte";
   import BtnPaste from "./BtnPaste.svelte";
+  import Advertising from "../components/Advertising.svelte";
 
   onMount(() => {
     document.addEventListener("keyup", (e) => {
@@ -36,25 +37,25 @@
   <BtnNumber id="zero" num="0" />
   <BtnBackSpace />
   <BtnOpenChat />
+  <Advertising />
 </section>
 
 <style>
   .form {
     display: grid;
     grid-template-columns: repeat(3, 6.62rem);
-    grid-template-rows: auto repeat(4, 1fr) auto;
+    grid-template-rows: auto repeat(4, 5rem) auto;
     gap: 5px;
     justify-content: center;
+    align-items: center;
   }
 
   #display {
     grid-column: 1/4;
-    height: 70px;
     padding: 10px;
-    font-size: 48px;
+    font-size: 60px;
     text-align: center;
     border: none;
-    margin-top: 10px;
   }
 
   #display:focus-visible {
