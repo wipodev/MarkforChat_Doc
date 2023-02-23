4fbefd6358b9c970c4393cb1284b5ef824ddcc05
wipodev <ajwipo@gmail.com>
2023-02-22 19:25:07 -0500

    Change of title and description, update of urls and expansion of the menu

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,15 @@
 export default {
-  title: "Mark for Chat",
-  titleTemplate: ":title",
-  description: "Just playing around.",
+  title: "WipoDev",
+  titleTemplate: "WipoDev | :title",
+  description:
+    "aplicación minimalista que te permite comunicarte con personas en WhatsApp sin necesidad de tener su número de teléfono guardado en tu libreta de contactos",
   lastUpdated: true,
-  base: "/markforchat_docs/",
+  base: "/markforchat/",
+  head: [["link", { rel: "icon", href: "/logo.svg", type: "image/svg" }]],
   themeConfig: {
-    siteTitle: "Mark for Chat",
-    logo: "/logo.svg",
-    socialLinks: [{ icon: "github", link: "https://github.com/AJ-Wi" }],
+    siteTitle: "",
+    logo: "/logo-main.svg",
+    socialLinks: [{ icon: "github", link: "https://github.com/wipodev" }],
   },
   locales: {
     root: {
@@ -15,12 +17,14 @@ export default {
       lan: "en",
       themeConfig: {
         nav: [
-          { text: "Home", link: "/" },
+          { text: "Home", link: "https://www.wipodev.com" },
+          { text: "Mark for Chat", link: "/" },
           { text: "APP Web", link: "/appweb/" },
+          { text: "Privacy Policy", link: "/privacy/" },
         ],
         footer: {
           message: "Disclaimer: This app is not affiliated with WhatsApp Inc.",
-          copyright: 'Copyright © 2023-present <a href="https://github.com/AJ-Wi">AJ-Wi</a>',
+          copyright: 'Copyright © 2023-present <a href="https://github.com/wipodev">WipoDev</a>',
         },
       },
     },
@@ -30,12 +34,14 @@ export default {
       link: "/es/",
       themeConfig: {
         nav: [
-          { text: "Inicio", link: "/es/" },
+          { text: "Inicio", link: "https://www.wipodev.com/es/" },
+          { text: "Mark for Chat", link: "/es/" },
           { text: "APP Web", link: "/es/appweb/" },
+          { text: "Políticas de Privacidad", link: "/es/privacy/" },
         ],
         footer: {
           message: "Descargo de responsabilidad: esta aplicación no está afiliada a WhatsApp Inc.",
-          copyright: 'Copyright © 2023-presente <a href="https://github.com/AJ-Wi">AJ-Wi</a>',
+          copyright: 'Copyright © 2023-presente <a href="https://github.com/wipodev">WipoDev</a>',
         },
       },
     },
